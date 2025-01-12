31e591407e9f4a137208ddcbc34b79ef0db8733d
okechukwu david <david@koonage.com>
2025-01-12 13:54:36 +0100

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,89 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import GWSIMAGE from "./GWSIMAGE.jpg";
+import Trello from "./trello.svg";
+import Slack from "./slack.svg";
+import Jira from "./jira.svg";
+import Notion from "./notion.svg";
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import WIZEBOOK from "./WIZEBOOK.jpg";
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import googledocs from "./googledocs.svg";
+import googledrive from "./googledrive.svg";
+import Miro from "./miro.svg";
+import Zoom from "./zoom.svg";
+import GoogleAnalytics from "./googleanalytics.svg";
+import Emeka from "./DAN_7400.jpg";
+import book from "./book1.jpg";
+import dami from "./dami.jpg";
+import OKEY from "./OKEY.jpg";
+export {
+  logo,
+  Trello,
+  WIZEBOOK,
+  Emeka,
+  dami,
+  OKEY,
+  book,
+  GoogleAnalytics,
+  Miro,
+  Zoom,
+  googledrive,
+  googledocs,
+  Jira,
+  Slack,
+  Notion,
+  GWSIMAGE,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,285 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  WIZEBOOK,
+  GWSIMAGE,
+  Trello,
+  Jira,
+  Notion,
+  Slack,
+  googledocs,
+  googledrive,
+  Miro,
+  Zoom,
+  GoogleAnalytics,
+  OKEY,
+  Emeka,
+  book,
+  dami,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Project Management",
+    icon: web,
+  },
+  {
+    title: "Product Management",
+    icon: mobile,
+  },
+  {
+    title: "Communication Strategist",
+    icon: backend,
+  },
+  {
+    title: "Team Building",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: Trello,
+  },
+  {
+    name: "CSS 3",
+    icon: Jira,
+  },
+  {
+    name: "JavaScript",
+    icon: Notion,
+  },
+  {
+    name: "TypeScript",
+    icon: Slack,
+  },
+  {
+    name: "React JS",
+    icon: googledocs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: googledrive,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: Miro,
+  },
+  {
+    name: "Node JS",
+    icon: Zoom,
+  },
+  // {
+  //   name: "MongoDB",
+  //   icon: mongodb,
+  // },
+  // {
+  //   name: "Three JS",
+  //   icon: threejs,
+  // },
+  {
+    name: "git",
+    icon: GoogleAnalytics,
+  },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
+];
+
+const experiences = [
+  {
+    title: "IT Project Manager | Agile Strategist",
+    company_name: "Rayzer Business and Technologies | Remote",
+    icon: web,
+    iconBg: "#E6DEDD",
+    date: "JUNE 2024 - PRESENT",
+    points: [
+      "Spearheaded the development and management of Rayzer's digital marketing platforms, driving social media engagement and expanding brand awareness.",
+      "Led the website development process, ensuring a seamless and optimized user experience for visitors, aligned with business objectives and customer needs.",
+      "Managed marketing campaigns for the Business Growth Accelerator book and Wizebook software, using targeted advertisements to drive sales and increase product visibility.",
+      "Oversaw modification and enhancement of the Wizebook inventory management software, improving operational efficiency and user functionality.",
+      "Conducted due diligence and qualification of business owners for the platform, ensuring high-quality partnerships and long-term success.",
+      "Utilized Jira, Notion, Smartsheet, Canva, GitHub, Google docs for tracking project progress, managing resources and risk, and maintaining timelines, enhancing transparency and productivity across teams.",
+    ],
+  },
+  {
+    title: "Project/Product Manager",
+    company_name: "Graduate Work Force Solution | Hybrid",
+    icon: web,
+    iconBg: "#383E56",
+    date: "SEPTEMBER 2023 - PRESENT",
+    points: [
+      "Managed the end-to-end process of graduate training programs, ensuring smooth onboarding and matching of graduates with business owners.",
+      "Developed and executed digital marketing strategies, increasing brand visibility and engagement through social media, targeted ad campaigns, and email marketing.Led the creation of a user-friendly website, improving user experience (UX) and site functionality for both graduates and business owners.",
+      "Coordinated the development of training content and resources, ensuring alignment with industry standards and business needs.",
+      "Implemented Agile methodologies to streamline project timelines, optimize resource allocation, and improve team collaboration for continuous improvement",
+      "Utilized Trello, Jira, zoom and google Doc to track progress, manage resources, and ensure compliance with project timelines, enhancing team productivity.",
+      "Led the creation of a user-friendly website, improving user experience (UX) and site functionality for both graduates and business owners.",
+    ],
+  },
+  {
+    title: "Project Manager",
+    company_name: "Bible Brains | Contract",
+    icon: web,
+    iconBg: "#383E56",
+    date: "JUNE 2023 - AUG 2023",
+    points: [
+      "Managed the annual Bible study program for teens, overseeing online and physical events with participants nationwide.",
+      "Led cross-functional teams in Event Coordination, Content Development, Marketing, and Technical Support to ensure seamless event execution.",
+      "Coordinated the creation of Bible study materials and quiz content, ensuring educational alignment and clarity.",
+      "Oversaw virtual event logistics, facilitating online participation and coordinating the physical location for Lagos participants.",
+      "Directed sponsorship acquisition, securing scholarships and prizes to engage participants and enhance event experience.",
+      "  Tools Used:Trello, Google Meet, Zoom, Slack, Google Docs, smartsheet",
+    ],
+  },
+  {
+    title: "Senior Technical Support Engineer",
+    company_name: "Tek Experts Nigeria | Onsite",
+    icon: web,
+    iconBg: "#E6DEDD",
+    date: "FEBRUARY 2021 - MAY 2023",
+    points: [
+      "Provided advanced technical support for enterprise clients, troubleshooting complex issues with Microsoft products and escalating cases as needed.",
+      " Collaborated with global support teams to resolve critical issues promptly,maintaining high service delivery standards and customer satisfaction.",
+      "Built strong client relationships through clear communication, regular status updates,and proactive insights, leading to repeat business requests from key accounts.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Omoye Iyoha has excelled as a Project Manager at Rayzer Business Technologies, showcasing exceptional leadership, organizational skills, and a commitment to delivering outstanding results. Her ability to manage complex projects, coordinate teams, and execute seamlessly has been crucial to our success. Omoye’s solutions-oriented mindset, attention to detail, and dedication to deadlines make her a valuable asset. She promotes a collaborative environment, motivates others, and continually seeks improvement. Her strong work ethic and passion for excellence have significantly impacted the company’s growth, and I highly recommend her for any leadership or project management role.",
+    name: "Emeka Ezefeika",
+    designation: "CEO",
+    company: "Rayzer Business and Technologies",
+    image: Emeka,
+  },
+  {
+    testimonial:
+      "Omoye Iyoha is an exemplary professional whose character and expertise as an IT Project Manager stand out.In the time I’ve known Omoye, she has demonstrated a remarkable ability to lead projects with integrity, ensuring that tasks are not only completed efficiently but also with a high standard of quality. She has a calm demeanor.Beyond their technical expertise, Omoye brings exceptional communication and team-building skills, fostering collaboration and motivating their teams to achieve outstanding results. Her dedication to continuous improvement and their honest, people-centered approach make them a great asset to any organization.",
+    name: "Damilola Olufemi ",
+    designation: "CE0, Lawyer & Thought Leader",
+    company: "Founder at TPELI Institute & Hope Foundation",
+    image: dami,
+  },
+  {
+    testimonial:
+      "A very dedicated and hardworking individual, she has a strong attention to detail ,also a strong problem solving skill and people oriented.",
+    name: "Okechukwu David",
+    designation: "Software Developer",
+    company: "Rayzer Business Technologies",
+    image: OKEY,
+  },
+];
+
+const projects = [
+  {
+    name: "WIZE BOOK",
+    description:
+      "Imagine a business grappling with inefficiencies in tracking inventory. That’s where Smart Business Manager, also known as Wizebook, steps in. As the lead project manager, I spearheaded a comprehensive transformation of the software, improving its adaptability and user experience. Through close collaboration with developers, we created a reliable solution that simplified operations for numerous businesses. The result? Exceptional feedback and a significantly optimized inventory system.",
+    tags: [
+      {
+        name: "Project Manager",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Product Manager",
+        color: "green-text-gradient",
+      },
+      // {
+      //   name: "",
+      //   color: "pink-text-gradient",
+      // },
+    ],
+    image: WIZEBOOK,
+    source_code_link: "https://rayzerbusiness.com/",
+  },
+  {
+    name: "Graduate workforce solutions",
+    description:
+      "As the project/product manager, I led my team in creating a landing page that connected graduates with businesses effortlessly. By focusing on a user-friendly design and streamlining the onboarding process, we made it easier for both businesses and graduates to connect. With Jira for project management and Google Analytics for tracking, we achieved a 40% increase in applications and successful matches in just three months. It was a rewarding experience, and this success was all about teamwork and thoughtful execution.",
+    tags: [
+      {
+        name: "Project manager",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Product manager",
+        color: "green-text-gradient",
+      },
+      // {
+      //   name: "PM",
+      //   color: "pink-text-gradient",
+      // },
+    ],
+    image: GWSIMAGE,
+    source_code_link: "https://gws.rayzerbusiness.com/",
+  },
+  {
+    name: "Business Growth Accelerator",
+    description:
+      "As the project/product manager, I led the development and launch of the Business Growth Accelerator book. My team and I worked together to create a comprehensive resource aimed at helping entrepreneurs scale their businesses effectively. From concept to execution, I ensured that every detail resonated with the target audience. The result was a book that provided actionable insights, empowering business owners to achieve sustainable growth. Leading this project was a truly fulfilling experience, and seeing its impact on entrepreneurs has been incredibly rewarding.",
+    tags: [
+      {
+        name: "product manager",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Project Manager",
+        color: "green-text-gradient",
+      },
+      // {
+      //   name: "PM",
+      //   color: "pink-text-gradient",
+      // },
+    ],
+    image: book,
+    source_code_link: "#",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
